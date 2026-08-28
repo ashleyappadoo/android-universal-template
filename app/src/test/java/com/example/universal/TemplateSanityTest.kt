2faad086a6e77c0c2ce7bfba6bf3fe73aa502095
ashleyappadoo <125403028+ashleyappadoo@users.noreply.github.com>
2026-08-28 11:57:43 +0200

    Add sanity test

---
+++ app/src/test/java/com/example/universal/TemplateSanityTest.kt
@@ -0,0 +1,11 @@
+package com.example.universal
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class TemplateSanityTest {
+    @Test
+    fun sanity() {
+        assertTrue(true)
+    }
+}
